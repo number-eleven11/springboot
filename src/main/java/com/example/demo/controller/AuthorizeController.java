fdfd68a46add70712c5d917c3450787e2cc0bbd5
ShiYihao <1635219023@qq.com>
2021-11-03 20:59:32 +0800

    add index

---
+++ src/main/java/com/example/demo/controller/AuthorizeController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import com.example.demo.provider.GiteeProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GiteeProvider giteeProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code, @RequestParam(name="state") String state){
+
+        return "index";
+    }
+}
---
+++ src/main/java/com/example/demo/provider/GiteeProvider.java
@@ -0,0 +1,10 @@
+package com.example.demo.provider;
+
+
+import org.springframework.stereotype.Component;
+
+///IOC
+@Component
+public class GiteeProvider {
+
+}
